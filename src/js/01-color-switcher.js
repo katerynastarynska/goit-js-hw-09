9f14e42475d2340e1abb25d9e086e02cb91e938e
Kateryna Starynska <katerynastarynska@gmail.com>
2022-07-12 16:24:23 -0500

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtn = document.querySelector('button[data-start]');
+console.log(startBtn);
+const stopBtn = document.querySelector('button[data-stop]');
+console.log(stopBtn);
+const bodyEl = document.querySelector("body");
+console.log(bodyEl);
 
+
+// startBtn.addEventListener("click", getRandomHexColor);
+startBtn.addEventListener("click", () => {
+    timerId = setInterval(getRandomHexColor, 1000);
+  });
+
+
+stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+});  
+
+function getRandomHexColor() {
+    bodyEl.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    // return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    console.log(bodyEl);
+  }
+ 
