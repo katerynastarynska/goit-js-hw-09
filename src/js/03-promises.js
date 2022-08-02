3b693e3b752a5926e376e134d9ac27507da03202
Kateryna Starynska <katerynastarynska@gmail.com>
2022-08-01 23:03:52 -0500

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form');
+const delayEl = document.querySelector('input[name="delay"]');
+const stepEl = document.querySelector('input[name="step"]');
+const amountEl = document.querySelector('input[name="amount"]');
+
+formEl.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(event) {
+  event.preventDefault();
+  let step = parseInt(delayEl.value);
+
+  for (let i = 1; i <= amountEl.value; i += 1) {
+    if (i !== 1) {
+      step += parseInt(stepEl.value);
+  }
+    createPromise(i, step);
   }
 }
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    setInterval(() => {
+    if (shouldResolve) {
+      resolve();
+    } else {
+      reject();
+    }}, delay)
+  }).then(() => {
+    Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+  }).catch(() => {
+    Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+  })
+}
+
+Notiflix.Notify.init({
+  width: '280px',
+  position: 'right-top',
+  distance: '10px',
+  opacity: 1,
+})
